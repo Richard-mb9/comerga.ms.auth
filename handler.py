31711ff8ea1bc391a5b8f7324115e564b47c03cb
richardmb9 <richard_mbs@hotmail.com>
2021-11-21 17:54:58 -0300

    ajustes

--- handler.py
+++ handler.py
@@ -4,8 +4,8 @@ from flask_lambda import FlaskLambda
 
 app = FlaskLambda(__name__)
 
-@app.route("/auth")
-def handler(event, context):
+@app.route("/auth", methods=['GET', 'POST'])
+def handler():
     body = {
         "message": "esta no ar",
     }
@@ -16,3 +16,7 @@ def handler(event, context):
     }
 
     return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
